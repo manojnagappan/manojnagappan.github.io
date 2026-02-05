51f393c81f3a54903a8b670e3918416ea746241f
Manoj Nagappan <manojnu04@gmail.com>
2026-02-05 13:19:37 +0530

    Initial content

---
+++ script.js
@@ -0,0 +1,10 @@
+function openTab(event, id) {
+    const contents = document.querySelectorAll('.content');
+    const tabs = document.querySelectorAll('.tab');
+
+    contents.forEach(c => c.classList.remove('active'));
+    tabs.forEach(t => t.classList.remove('active'));
+
+    document.getElementById(id).classList.add('active');
+    event.target.classList.add('active');
+}
